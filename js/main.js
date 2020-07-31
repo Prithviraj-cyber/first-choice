62c69413a49464accd60210e1aba49cb9bca101b
Prithviraj-cyber <anandsinghchauhan03646@gmail.com>
2020-07-31 10:20:04 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//----------popup----------//
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+  });
+});
+
+//-----------Account form--------//
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () =>
+	container.classList.add("right-panel-active")
+);
+
+signInButton.addEventListener('click', () => 
+	container.classList.remove("right-panel-active")
+);
